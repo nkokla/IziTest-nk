86b2498b69b7c8d6f5af76f88dc1ed1fb610be2a
Nicolas KOKLA <n.kokla@koaji.fr>
2021-06-08 23:04:30 +0200

    ✨ Add Filter Modal

---
+++ src/components/DataGrid/FilterModal.styled.tsx
@@ -0,0 +1,71 @@
+import styled, { css } from "styled-components";
+
+export const Modal = styled.div<{ ref: any }>`
+  width: 345px;
+  height: 230px;
+  background-color: #fff;
+  box-shadow: 0 1px 6px rgba(147, 163, 192, 0.5);
+  border-radius: 4px;
+  box-sizing: border-box;
+  padding: 2em;
+  margin: 0.5em;
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  justify-content: flex-start;
+  font-weight: 500;
+  position: absolute;
+  left: 0;
+  z-index: 1;
+`;
+
+export const ModalTitle = styled.div`
+  font-weight: bold;
+  margin: 1em 0;
+`;
+
+export const InputQuery = styled.input.attrs({ type: "text" })`
+  border: 1px solid #196cff;
+  border-radius: 4px;
+  padding: 1em;
+`;
+
+export const Label = styled.div`
+  cursor: default;
+`;
+const RadioMark = styled.div<{ isChecked?: Boolean }>`
+  width: calc(100%);
+  height: calc(100%);
+  background-color: ${({ isChecked }) =>
+    isChecked ? `#196cff` : `transparent`};
+  border: 3px solid #fff;
+  border-radius: 50%;
+  box-sizing: border-box;
+  transition: background-color ease 250ms;
+`;
+export const Radio = styled.div.attrs<{ value: string; currentValue: string }>(
+  ({ value, currentValue }) => ({
+    children: <RadioMark isChecked={value === currentValue} />,
+    isChecked: value === currentValue,
+  })
+)<{ currentValue: string; value: string }>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 18px;
+  height: 18px;
+  background-color: #fff;
+  border: 1px solid #56709c;
+  border-radius: 50%;
+  box-sizing: border-box;
+  margin: 0 0.5em -1px 0;
+
+  transition: border ease 250ms, margin ease 250ms;
+
+  ${({ value, currentValue }) =>
+    value === currentValue &&
+    css`
+      border: 2px solid #196cff;
+      margin-bottom: -2px;
+    `}
+`;
--- src/components/DataGrid/helper.ts
+++ src/components/DataGrid/helper.ts
@@ -6,3 +6,59 @@ export type filterType = {
   key: string;
   sort: "date" | "az" | "za";
 };
+
+export const initialFilter: filterType = {
+  modal: "",
+  key: "",
+  query: "",
+  sort: "date",
+};
+
+export const filterReducer = (state: any, action: any): filterType => {
+  switch (action.type) {
+    case "open-modal":
+      return { ...state, modal: action.modal };
+    case "close-modal":
+      return { ...state, modal: "" };
+    case "set-key": {
+      const { modal, ...resetFilter } = initialFilter;
+      return {
+        ...state,
+        ...(action.reset ? resetFilter : {}),
+        key: action.key,
+      };
+    }
+    case "set-query":
+      return { ...state, query: action.query };
+    case "set-sort":
+      return { ...state, sort: action.sort };
+    default:
+      throw new Error();
+  }
+};
+
+const sortFunc: any = {
+  az: (valA: string, valB: string) => {
+    if (valA < valB) return -1;
+    if (valA > valB) return 1;
+    return 0;
+  },
+  za: (valA: string, valB: string) => {
+    if (valB < valA) return -1;
+    if (valB > valA) return 1;
+    return 0;
+  },
+  date: (valA: string, valB: string) => {
+    const dateA: Date = new Date(valA);
+    const dateB: Date = new Date(valB);
+    if (dateA < dateB) return -1;
+    if (dateA > dateB) return 1;
+    return 0;
+  },
+};
+
+export const sorter = (key: string, type: string) => (a: any, b: any) => {
+  const valA: string = a?.[key];
+  const valB: string = b?.[key];
+  return sortFunc?.[type]?.(valA, valB);
+};
