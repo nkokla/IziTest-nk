8ab9341d7c32d24a6eff7d74c1d9f2ecbc892f23
Nicolas KOKLA <n.kokla@koaji.fr>
2021-06-08 22:33:16 +0200

    FIX -- ✨ DataGrid component integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 
 import { DataGrid } from "./components";
-import { dataHead, dataSet } from "./fakeData";
+import { dataSet } from "./fakeData";
 
 function App() {
   return (
     <div className="App">
-      <DataGrid headersData={dataHead} data={dataSet} />
+      <DataGrid data={dataSet} />
     </div>
   );
 }
